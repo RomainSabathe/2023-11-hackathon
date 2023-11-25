1c0ee3304932561fccf31328ea0764ff7cc40e26
Romain Sabathe <RSabathe@gmail.com>
2023-11-25 20:18:58 +0000

    Fixing the diffusion bug

--- diffusion.py
+++ diffusion.py
@@ -1,11 +1,14 @@
+import argparse
 from pathlib import Path
+from math import ceil, sqrt
 from typing import Optional
 
+import torch
+import wandb
 import torch
 import numpy as np
 from PIL import Image
 from tqdm.auto import tqdm
-import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision.transforms import v2 as tvtf
@@ -25,130 +28,115 @@ OUTPUT_IMAGES_PATH.mkdir(exist_ok=True, parents=True)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-def custom_ddpm():
-    T = 1_000  # like in the paper.
-    beta_1, beta_T = 1e-4, 0.02
-    dataset_root = Path("data") / "fashion_mnist"
-    dataset = FashionMNIST(
-        dataset_root,
-        download=not dataset_root.exists(),
+class Subset:
+    def __init__(self, dataset, n):
+        self.dataset = dataset
+        self.n = n
+
+    def __len__(self):
+        return self.n
+
+    def __getitem__(self, i):
+        return self.dataset[i]
+
+
+def custom_ddpm(
+    notes: Optional[str] = None, debug: bool = False, skip_training: bool = False
+):
+    wandb.init(
+        project="diffusion-customer-ddpm",
+        config=dict(
+            # Diffusion params
+            T=1_000,  # like in the paper.
+            beta_1=1e-4,
+            beta_T=0.02,
+            # Dataset params
+            dataset_root=Path("data") / "fashion_mnist",
+            use_dataset_subset=False,
+            # Network architecture params
+            model_block_out_channels=(32, 64, 128, 128),
+            # Training params
+            learning_rate=1e-3,
+            n_epochs=20,
+            n_iter_per_epoch=None,
+            skip_training=skip_training,
+            batch_size=128,
+            # Misc params
+            seed=3,
+            device=device,
+            tags=("debug") if debug else None,
+        ),
+        notes=notes,
+    )
+
+    # Initialization
+    torch.manual_seed(wandb.config.seed)
+
+    # Dataset loading
+    full_dataset = FashionMNIST(
+        wandb.config.dataset_root,
+        download=not Path(wandb.config.dataset_root).exists(),
         transform=tvtf.Compose(
             [
                 tvtf.ToImage(),
                 tvtf.ToDtype(torch.float32, scale=True),
                 tvtf.Resize((32, 32), antialias=True),
-                # lambda x: (x * 2.0) - 1.0,
-                lambda x: (x * 1.0) - 0.5,
+                lambda x: (x * 2.0) - 1.0,
             ]
         ),
     )
+    dataset = (
+        full_dataset
+        if not wandb.config.use_dataset_subset
+        else Subset(full_dataset, n=wandb.config.batch_size)
+    )
 
-    class Subset:
-        def __init__(self, dataset, n):
-            self.dataset = dataset
-            self.n = n
-
-        def __len__(self):
-            return self.n
-
-        def __getitem__(self, i):
-            return self.dataset[i]
-
-    # dataset = Subset(dataset, 4)
-
-    scale_factor = 10
-    batch_size = 64 * scale_factor
-    batch_size = 128
-
-    # batch_size = 4
-
-    learning_rate = 4e-3
-    n_epochs = 5
-    n_iter_per_epoch = 9999999
-    seed = 1
-    skip_training = True
-
-    torch.manual_seed(seed)
-
+    # Model loading
     sample_image, sample_label = dataset[0]
     C, H, W = sample_image.shape
     model = UNet2DModel(
         sample_size=(H, W),
         in_channels=C,
         out_channels=C,
-        block_out_channels=(32, 64, 128, 128),
+        block_out_channels=wandb.config.model_block_out_channels,
     ).to(device)
 
+    # Initializing the DDPM constants that are used for forward and reverses processes.
     with torch.no_grad():
-        beta = torch.linspace(beta_1, beta_T, steps=T).to(device)
+        beta = torch.linspace(
+            wandb.config.beta_1, wandb.config.beta_T, steps=wandb.config.T
+        ).to(device)
         alpha = 1 - beta
         alpha_bar = torch.cumprod(alpha, dim=0)
 
-    if not skip_training:
-
-        def q_given_x0(ts, x0s) -> torch.Tensor:
-            # ts has shape (B,) and dtype torch.int
-            # x0s has shape (B, C, H, W) and dtype torch.float
-            B, C, H, W = x0s.shape
-            _alpha_bar = alpha_bar[ts].view(B, 1, 1, 1)
-            return torch.normal(
-                mean=torch.sqrt(_alpha_bar) * x0s,
-                std=torch.sqrt(1 - _alpha_bar),
-            )
-
-        # Not used in practice since q_given_x0 is a generalisation of this formula.
-        def q_given_xtm1(t, xtm1) -> torch.Tensor:
-            return torch.normal(mean=torch.sqrt(1 - beta(t)) * xtm1, std=beta(t))
-
-        # Checking it all works
-        with torch.no_grad():
-            ts = torch.tensor([0, 10, 100, 500, 800, 999]).to(device)
-            noisy_sample_images = q_given_x0(
-                ts, torch.stack([sample_image] * len(ts), dim=0).to(device)
-            )
-            # noisy_sample_images = ((noisy_sample_images + 1.0) / 2.0).clamp(0, 1)
-            noisy_sample_images = ((noisy_sample_images - 0.5) / 1.0).clamp(-0.5, 0.5)
-            for t, noisy_sample_image in zip(ts, noisy_sample_images):
-                tvtf.ToPILImage()(noisy_sample_image).save(
-                    OUTPUT_IMAGES_PATH / f"forward_pass_{t:04d}.jpg"
-                )
-
-        # reminder:
-        # forward process goes from image to noise
-        #                             0   to  T
-        # backward process goes from noise to image
-        #                             T   to  0
-        # algo:
-        # 1. get a batch of images Xs and
-        #    sample different timesteps Ts
-        # 2. for each X, compute the forward process with the respective T and T+1
-        # (we will train the model to perform the backward process from T+1 to T)
-        # forward(x, t) = q_given_x0(t, x)
-        # forward(x, t+1) = q_given_x0(t+1, x)
-        # 3. Determine the noise pattern that was added during the forward step from T to T+1:
-        # noise = forward(x, t+1) - forward(x, t)
-        # 4. Get the prediction of the UNet from the image at step t+1
-        # predicted_noise = UNet(x(t+1)) = UNet(forward(x, t+1))
-        # 5. Minimise the MSE
-        # grad((noise - predicted_noise)**2.)
-
-        # Training loop
+    # Training loop (i.e. forward process).
+    if not wandb.config.skip_training:
         loss_fn = nn.MSELoss()
-        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+        optimizer = torch.optim.Adam(model.parameters(), lr=wandb.config.learning_rate)
         scheduler = torch.optim.lr_scheduler.OneCycleLR(
-            optimizer, max_lr=learning_rate, total_steps=n_epochs * len(dataset)
+            optimizer,
+            max_lr=wandb.config.learning_rate,
+            total_steps=ceil(len(dataset) / wandb.config.batch_size)
+            * wandb.config.n_epochs,
         )
-        for epoch in tqdm(range(n_epochs)):
+
+        for epoch in tqdm(range(wandb.config.n_epochs)):
             for i, (x0s, _) in enumerate(
-                tqdm(DataLoader(dataset, batch_size=batch_size, shuffle=True))
+                DataLoader(dataset, batch_size=wandb.config.batch_size, shuffle=True)
             ):
-                if i >= n_iter_per_epoch:
+                if (
+                    wandb.config.n_iter_per_epoch is not None
+                    and i >= wandb.config.n_iter_per_epoch
+                ):
                     break
 
                 B, C, H, W = x0s.shape
-                # with torch.no_grad():
                 ts = torch.randint(
-                    low=0, high=T - 1, size=(B,), dtype=torch.long, device=device
+                    low=0,
+                    high=wandb.config.T - 1,
+                    size=(B,),
+                    dtype=torch.long,
+                    device=device,
                 )
                 x0s = x0s.to(device)
 
@@ -158,97 +146,71 @@ def custom_ddpm():
                     + torch.sqrt(1 - alpha_bar[ts]).view(B, 1, 1, 1) * noises
                 )
 
-                # xts = q_given_x0(ts, x0s)
-                # xtp1s = q_given_x0(ts + 1, x0s)
-                # noises = xtp1s - xts
                 predicted_noises = model(xtp1s, ts).sample
                 loss = loss_fn(predicted_noises, noises)
+
+                wandb.log(
+                    {
+                        "train_loss": loss,
+                        "learning_rate": optimizer.param_groups[0]["lr"],
+                    }
+                )
 
                 optimizer.zero_grad(set_to_none=True)
                 loss.backward()
                 optimizer.step()
                 scheduler.step()
 
-                if i % 25 == 0:
-                    print(loss)
-
         torch.save(model.cpu().state_dict(), "/tmp/unet.bin")
 
-    # Reverse process now.
-    # recall: p(x_t-1|x_t) ~ N(mu_t, sigma_t) with:
-    # sigma**2_t = beta_t or beta_tilde_t
-    # mu_t = complicated formula
+    # Reverse process
     model.load_state_dict(torch.load("/tmp/unet.bin"))
     model = model.to(device).eval()
-    batch_size = 4
-
-    @torch.no_grad()
-    def sample(model, sz=(1, 1, 32, 32)):
-        n_steps = 1_000
-        βmin = 1e-4
-        βmax = 0.02
-
-        device = next(model.parameters()).device
-        x_t = torch.randn(sz, device=device)
-        β = torch.linspace(βmin, βmax, n_steps)
-        α = 1.0 - β
-        ᾱ = torch.cumprod(α, dim=0)
-        σ = β.sqrt()
-
-        for t in tqdm(reversed(range(n_steps))):
-            t_batch = torch.full((x_t.shape[0],), t, device=device, dtype=torch.long)
-            z = (torch.randn(x_t.shape) if t > 0 else torch.zeros(x_t.shape)).to(device)
-            ᾱ_t1 = ᾱ[t - 1] if t > 0 else torch.tensor(1)
-            b̄_t = 1 - ᾱ[t]
-            b̄_t1 = 1 - ᾱ_t1
-            noise_pred = model(x_t, t_batch).sample
-            # x_0_hat = ((x_t - b̄_t.sqrt() * noise_pred) / ᾱ[t].sqrt()).clamp(-1, 1)
-            x_0_hat = ((x_t - b̄_t.sqrt() * noise_pred) / ᾱ[t].sqrt()).clamp(-0.5, 0.5)
-            x0_coeff = ᾱ_t1.sqrt() * (1 - α[t]) / b̄_t
-            xt_coeff = α[t].sqrt() * b̄_t1 / b̄_t
-            x_t = x_0_hat * x0_coeff + x_t * xt_coeff + σ[t] * z
-
-            if t % 100 == 0 or (t < 100 and t % 10 == 0):
-                img = x_t[0]
-                # img = ((img + 1.0) / 2.0).clamp(0, 1)
-                img = ((img + 0.5) / 1.0).clamp(-0.5, 0.5)
-                tvtf.ToPILImage()(img).save(
-                    OUTPUT_IMAGES_PATH / f"reverse_pass_{t:04d}.jpg"
-                )
-        return x_t
-
-    la = sample(model)
-    return
+    batch_size = wandb.config.batch_size
 
     with torch.no_grad():
         xts = torch.randn((batch_size, C, H, W)).to(device)
-        for t in tqdm(reversed(range(T))):
-            ts = t * torch.ones((batch_size, 1, 1, 1), device=device, dtype=torch.long)
-            z = (
-                torch.randn_like(xts, device=device)
-                if t > 0
-                else torch.zeros_like(xts, device=device)
-            )
+        for t in tqdm(reversed(range(wandb.config.T))):
+            ts = torch.full((batch_size, 1, 1, 1), t, device=device, dtype=torch.long)
+            z = torch.randn_like(xts) if t > 0 else torch.zeros_like(xts)
 
-            std = torch.sqrt(beta[ts])
+            _alpha = alpha[ts]
+            _beta = beta[ts]
+            _alpha_bar = alpha_bar[ts]
+            _sigma = torch.sqrt(_beta)
 
             estimated_noise = model(xts, ts.view(batch_size)).sample
-            mu = alpha[ts] ** (-1 / 2) * (
-                xts - estimated_noise * (beta[ts] / torch.sqrt(1 - alpha_bar[ts]))
-            )
 
-            xts = (mu + std * z).clamp(-1, 1)
+            scaling_factor = 1 / torch.sqrt(_alpha)
+            noise_scaling_factor = (1 - _alpha) / torch.sqrt(1 - _alpha_bar)
+            mu = scaling_factor * (xts - noise_scaling_factor * estimated_noise)
 
-            if t % 100 == 0 or (t < 100 and t % 10 == 0):
-                img = xts[0]
-                img = ((img + 1.0) / 2.0).clamp(0, 1)
-                tvtf.ToPILImage()(img).save(
+            xts = mu + _sigma * z
+
+            if t % 250 == 0 or (t < 50 and t % 10 == 0) or t == 0:
+                torch_imgs_to_pil(xts).save(
                     OUTPUT_IMAGES_PATH / f"reverse_pass_{t:04d}.jpg"
                 )
 
-    import ipdb
+def torch_imgs_to_pil(tensor: torch.Tensor) -> Image:
+    # We assume tensor has:
+    # 1. values in [-1, 1]
+    # 2. shape (B, C, H, W)
+    B, C, H, W = tensor.shape
 
-    ipdb.set_trace()
+    # Finding the greatest divisors of B
+    rows, cols = 1, B
+    for i in reversed(range(1, int(sqrt(B)))):
+        if B % i == 0:
+            rows, cols = i, B // i
+            break
+    tensor = tensor.view(rows, cols, C, H, W)
+
+    irows, icols, iC, iH, iW = 0, 1, 2, 3, 4
+    tensor = tensor.permute(iC, irows, iH, icols, iW).reshape(C, rows * H, cols * W)
+
+    tensor = ((tensor + 1.0) / 2.0).clamp(0, 1)
+    return tvtf.functional.to_pil_image(tensor)
 
 
 def runway_stable_diffusion_v1_5():
@@ -427,4 +389,11 @@ def google_ddpm_cat_256():
 
 
 if __name__ == "__main__":
-    custom_ddpm()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--notes", type=str, help="Notes about the experiment")
+    parser.add_argument("--debug", action="store_true")
+    parser.add_argument("-s", "--skip-training", action="store_true")
+
+    args = parser.parse_args()
+
+    custom_ddpm(args.notes, args.debug, args.skip_training)
